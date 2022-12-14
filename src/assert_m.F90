b04e42065842b539549b1d4583ce12e3e09e1902
Damian Rouson <rouson@lbl.gov>
2022-12-14 15:04:06 -0800

    solve(exercise 1): first test passes

---
+++ src/assert_m.F90
@@ -0,0 +1,31 @@
+module assert_m
+  implicit none
+
+#ifndef ASSERTIONS
+# define ASSERTIONS .true.
+#endif
+
+  interface 
+
+     pure module subroutine assert(assertion, description, diagnostic_data)
+       implicit none
+       logical, intent(in) :: assertion
+       character(len=*), intent(in) :: description
+       real, intent(in) :: diagnostic_data
+     end subroutine
+
+  end interface 
+
+end module assert_m
+
+submodule(assert_m) assert_s
+  implicit none
+contains
+
+  module procedure assert
+     if (ASSERTIONS) then
+       if (.not. assertion) error stop "The assertion described by '" // description // "' fails."
+     end if
+  end procedure
+
+end submodule assert_s
