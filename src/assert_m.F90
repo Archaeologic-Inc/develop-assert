05c0f3d3927d8687979d92545582f1ec95539e30
Damian Rouson <rouson@lbl.gov>
2022-12-15 05:34:04 -0800

    solve(exercise-3): demo unlimited polymorphism`

--- src/assert_m.F90
+++ src/assert_m.F90
@@ -11,7 +11,7 @@ module assert_m
        implicit none
        logical, intent(in) :: assertion
        character(len=*), intent(in) :: description
-       real, intent(in), optional :: diagnostic_data
+       class(*), intent(in), optional :: diagnostic_data
      end subroutine
 
   end interface 
@@ -23,9 +23,38 @@ submodule(assert_m) assert_s
 contains
 
   module procedure assert
+     character(len=:), allocatable :: stop_code
      if (ASSERTIONS) then
-       if (.not. assertion) error stop "The assertion described by '" // description // "' fails."
+       if (.not. assertion) then 
+
+         if (.not. present(diagnostic_data)) then
+           stop_code = "Assertion '" // description // "' fails."
+         else
+           select type(diagnostic_data)
+             type is(integer)
+               stop_code = "Assertion '" // description // "' fails with diagnostic data " // string(diagnostic_data)
+             class default
+               error stop "Unrecognized diagnostic data"
+           end select
+         end if
+
+         error stop stop_code
+       end if
      end if
   end procedure
+
+  pure function string(i) result(string_)
+    integer, intent(in) :: i
+    integer :: io_status, length
+    character(len=:), allocatable :: string_
+    length = 1
+    do 
+      allocate(character(len=length) :: string_)
+      write(string_,*, iostat=io_status) i
+      if (io_status==0) return
+      deallocate(string_)
+      length = length + 1
+    end do
+  end function
 
 end submodule assert_s
